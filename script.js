61b04dec941c62c880ac46d175932f302f3a0d38
Fiorella Aguayo <fiorelamarina12@gmail.com>
2024-04-25 02:35:23 -0300

    subo proyecto y agrego interruptor

---
+++ script.js
@@ -0,0 +1,9 @@
+const toggleSwitch = document.getElementById('toggle-switch');
+
+toggleSwitch.addEventListener('change', function() {
+    if (this.checked) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+});
